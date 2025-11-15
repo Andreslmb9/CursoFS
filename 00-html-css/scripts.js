da6f6df23401049aa99e92427bf2d6004c17c9d6
andreslmb9 <andreslmb9@gmail.com>
2025-11-14 21:56:30 -0500

    Primeros pasos con JS, listener de elementos

--- 00-html-css/scripts.js
+++ 00-html-css/scripts.js
@@ -1,6 +1,51 @@
+// Recupera solo el primer botón que encuentra.
+// const botonUnico = document.querySelector('.button-apply-job')
+// console.log(botonUnico)
 
-        const boton = document.querySelector('#boton-importante')
+// if (botonUnico !== null) {
+//     botonUnico.addEventListener('click', () => {
+//         botonUnico.textContent = 'Aplicado!'
+//         botonUnico.classList.add('is-applied')
+//         botonUnico.disable = true
+//     })
+// }
 
-        boton.addEventListener('click', () =>{
-            alert('Gracias por tu interes en esta oferta de trabajo. Serás redirigido al sitio de la empresa para completar tu aplicación.')
-        })
+// Recupera todos los botones que coinciden con la clase dada.
+
+// Segunda forma de hacerlo:
+
+// const boton = document.querySelectorAll('.button-apply-job')
+
+// // Recorre el NodeList de botones y les añade un evento a cada uno.
+// // Puede devolver una lista vacía si no encuentra ningún elemento que coincida con el selector.
+
+// boton.forEach(boton => {
+//     boton.addEventListener('click', () => {
+//     boton.textContent = 'Aplicado!'
+//     boton.classList.add('is-applied')
+//     boton.disable = true
+// })
+// })
+
+const jobListingSection = document.querySelector('.job-listings')
+
+jobListingSection?.addEventListener('click', function (event) { // Optional chaining, valida si el elemento existe, si es null, no se ejecuta nada a la derecha del ?
+    const element = event.target
+
+    if (element.classList.contains('button-apply-job')) {
+        element.textContent = 'Aplicado!'
+        element.classList.add('is-applied')
+        element.disable = true
+    }
+});
+
+// Ejercicio 1: Crear la tercera pagina detalle de la oferta
+// Ejercicio 2: Filtrar las ofertas por tipo de trabajo (remoto, tiempo completo, medio tiempo), Pista, escuchando el evento 'change' en un select
+
+
+
+
+
+
+
+
